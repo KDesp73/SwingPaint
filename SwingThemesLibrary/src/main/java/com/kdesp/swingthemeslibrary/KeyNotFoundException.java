f9ea646eac1195c853daf484e9d73eecea274d9f
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2023-02-06 17:21:59 +0200

    YamlFile class added

---
+++ SwingThemesLibrary/src/main/java/com/kdesp/swingthemeslibrary/KeyNotFoundException.java
@@ -0,0 +1,12 @@
+
+package com.kdesp.swingthemeslibrary;
+
+/**
+ *
+ * @author Konstantinos
+ */
+class KeyNotFoundException extends RuntimeException{
+        public KeyNotFoundException(String err){
+                super(err);
+        }
+}
