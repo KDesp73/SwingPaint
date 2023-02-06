4f4dfdd91ba07a1bf87f70eae6e308fa31291c48
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2023-02-06 17:09:44 +0200

    get all data from yaml and get specific value done

--- SwingThemesLibrary/src/main/java/com/kdesp/swingthemeslibrary/SwingThemesLibrary.java
+++ SwingThemesLibrary/src/main/java/com/kdesp/swingthemeslibrary/SwingThemesLibrary.java
@@ -1,8 +1,36 @@
-
 package com.kdesp.swingthemeslibrary;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.yaml.snakeyaml.Yaml;
 
 public class SwingThemesLibrary {
 
-    public static void main(String[] args) {
-    }
+        public static void main(String[] args) throws IOException {
+                System.out.println(getValue("button").toString());
+        }
+
+        public static Map<String, Object> getYamlData(String fileDirectory) {
+                InputStream inputStream = null;
+                try {
+                        inputStream = new FileInputStream(new File(fileDirectory));
+                } catch (FileNotFoundException ex) {
+                        Logger.getLogger(SwingThemesLibrary.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                Yaml yaml = new Yaml();
+                Map<String, Object> data = yaml.load(inputStream);
+                return data;
+        }
+        
+        public static Object getValue(String key) throws IOException{
+                Map<String, Object> data = getYamlData(new java.io.File(".").getCanonicalPath() + "\\src\\main\\java\\com\\kdesp\\swingthemeslibrary\\" + "theme.yml");
+                return data.get(key);
+        }
 }
---
+++ SwingThemesLibrary/src/main/java/com/kdesp/swingthemeslibrary/ThemeInterface.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.kdesp.swingthemeslibrary;
+
+/**
+ *
+ * @author Konstantinos
+ */
+public interface ThemeInterface {
+        public String getName();
+        public void setName(String name);
+        public String getJSON();
+        public void setJSON(String json);
+}
